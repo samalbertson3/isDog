2352ea67fee0d8d2546025295644496b0cf54f07
Danielle Vaithilingam <dvait16@gmail.com>
2023-04-15 20:41:35 -0400

    messing around

--- tests/test_isdog.py
+++ tests/test_isdog.py
@@ -1,1 +1,8 @@
+"""Test Suite for isDog."""
+
 import pytest
+
+## Tests...
+## back end spits out percentages
+## front end handels user input -> send to back end & recieves percentages and gives correct output.
+## also correct error handling!!!
