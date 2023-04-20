683baffa226cddd6e7bc04349f19c8adb0b02993
Sam Albertson <samalbertson3@gmail.com>
2023-04-20 13:54:16 -0400

    fiddled with model complexity + added regularization

--- test_cnn.py
+++ test_cnn.py
@@ -1,17 +1,22 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras import layers, models, regularizers
 import tensorflow_datasets as tfds
+import numpy as np
+from PIL import Image
+
+# Define the input size of the images
+input_shape = (224, 224, 3)
 
 
-def train_model():
+def train_model(input_shape):
     # Load the Stanford Dogs dataset
     print("Loading dogs...")
-    dogs_ds, dogs_info = tfds.load("stanford_dogs", with_info=True, split="train[:80%]")
+    dogs_ds, dogs_info = tfds.load("stanford_dogs", with_info=True, split="train[:60%]")
 
     print("Loading non-dogs...")
     # Load the Caltech 101 dataset
     non_dogs_ds, non_dogs_info = tfds.load(
-        "caltech101", with_info=True, split="train[:80%]"
+        "caltech101", with_info=True, split="train[:60%]"
     )
 
     print("Subsetting data...")
@@ -41,30 +46,43 @@ def train_model():
     # Shuffle and batch the dataset
     dataset = dataset.shuffle(1024).batch(32).prefetch(tf.data.AUTOTUNE)
 
-    # Define the input size of the images
-    input_shape = (224, 224, 3)
-
     print("Building model...")
     # Create the model
     model = models.Sequential()
 
     # Add the convolutional layers
-    model.add(layers.Conv2D(3, (3, 3), activation="relu", input_shape=input_shape))
+    model.add(
+        layers.Conv2D(
+            32,
+            (3, 3),
+            activation="relu",
+            input_shape=input_shape,
+            kernel_regularizer=regularizers.L1(0.01),
+        )
+    )
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(4, (3, 3), activation="relu"))
+    model.add(
+        layers.Conv2D(
+            64, (3, 3), activation="relu", kernel_regularizer=regularizers.L1(0.01)
+        )
+    )
     model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(8, (3, 3), activation="relu"))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(8, (3, 3), activation="relu"))
-    model.add(layers.MaxPooling2D((2, 2)))
+    # model.add(layers.Conv2D(128, (3, 3), activation="relu"))
+    # model.add(layers.MaxPooling2D((2, 2)))
+    # model.add(layers.Conv2D(128, (3, 3), activation="relu"))
+    # model.add(layers.MaxPooling2D((2, 2)))
 
     # Add the flatten layer
     model.add(layers.Flatten())
 
     # Add the dense layers
-    model.add(layers.Dense(512, activation="relu"))
+    model.add(
+        layers.Dense(512, activation="relu", kernel_regularizer=regularizers.L1(0.01))
+    )
     model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(1, activation="sigmoid"))
+    model.add(
+        layers.Dense(1, activation="sigmoid", kernel_regularizer=regularizers.L1(0.01))
+    )
 
     print("Training model...")
     # Compile the model
@@ -75,3 +93,38 @@ def train_model():
 
     print("Done!")
     return model
+
+
+def preprocess_image(image_path):
+    # Load the image using Pillow
+    img = Image.open(image_path)
+    # Resize the image
+    img = img.resize(input_shape[:2])
+    # Convert the image to a numpy array
+    img_array = np.array(img)
+    # Expand the dimensions of the image to match the input shape of the model
+    img_array = np.expand_dims(img_array, axis=0)
+    # Scale the pixel values to be between 0 and 1
+    img_array = img_array / 255.0
+    return img_array
+
+
+def predict_image_class(image_path, model):
+    # Preprocess the input image
+    img_array = preprocess_image(image_path)
+    # Use the model to make a prediction
+    predictions = model.predict(img_array)
+    # Get the predicted class (1 for dog, 0 for non-dog)
+    raw_prediction = predictions[0]
+    predicted_class = int(np.round(raw_prediction))
+    # Return the predicted class
+    return predicted_class, raw_prediction
+
+
+model = train_model(input_shape)
+print(predict_image_class("C:/Users/Sam/Desktop/dog.jpg", model))
+print(predict_image_class("C:/Users/Sam/Desktop/dog2.jpg", model))
+print(predict_image_class("C:/Users/Sam/Desktop/dog3.jpg", model))
+print(predict_image_class("C:/Users/Sam/Desktop/non-dog.jpg", model))
+print(predict_image_class("C:/Users/Sam/Desktop/non-dog2.jpg", model))
+print(predict_image_class("C:/Users/Sam/Desktop/non-dog3.jpg", model))
