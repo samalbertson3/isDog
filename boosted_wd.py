c0566094bca4fbaea15af232a021f74975cb8a76
Dani <djv16@duke.edu>
2023-05-01 14:28:31 -0400

    wherever this left off

--- boosted_wd.py
+++ boosted_wd.py
@@ -1,15 +1,27 @@
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import accuracy_score
 from tensorflow.keras import layers, models, regularizers
+import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
 from PIL import Image
 
-
+IMG_SIZE = 224
+# Define the input size of the images
+input_shape = (224, 224, 3)
 # Load the dataset
 print("loading dogs")
 dataset, info = tfds.load('stanford_dogs', with_info=True)
 print("loaded dogs")
+
+def preprocess(features):
+    image = features['image']
+    image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
+    image = tf.cast(image, tf.float32) / 255.0
+    label = features['label']
+    return {'image': image, 'label': label}
+
+
 # Preprocess the data
 train_data = dataset['train'].map(preprocess)
 test_data = dataset['test'].map(preprocess)
@@ -20,12 +32,54 @@ train_labels = np.array([x['label'] for x in train_data])
 test_features = np.array([x['image'] for x in test_data])
 test_labels = np.array([x['label'] for x in test_data])
 print("training first model")
+
+# build model 
+model = models.Sequential()
+
+# Add the convolutional layers
+model.add(
+    layers.Conv2D(
+        32,
+        (3, 3),
+        activation="relu",
+        input_shape=input_shape,
+        kernel_regularizer=regularizers.L1(0.01),
+    )
+)
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(
+    layers.Conv2D(
+        64, (3, 3), activation="relu", kernel_regularizer=regularizers.L1(0.01)
+    )
+)
+model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(128, (3, 3), activation="relu"))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(128, (3, 3), activation="relu"))
+# model.add(layers.MaxPooling2D((2, 2)))
+
+# Add the flatten layer
+model.add(layers.Flatten())
+
+# Add the dense layers
+model.add(
+    layers.Dense(512, activation="relu", kernel_regularizer=regularizers.L1(0.01))
+)
+model.add(layers.Dropout(0.5))
+model.add(
+    layers.Dense(1, activation="sigmoid", kernel_regularizer=regularizers.L1(0.01))
+)
+
+print("Training model...")
+# Compile the model
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
 # Train the TensorFlow model on the training set
 model.fit(train_features, train_labels)
 
 print("training second model")
 # Use the trained TensorFlow model as a weak classifier in AdaBoost
-ada_boost_model = AdaBoostClassifier(base_estimator=model, n_estimators=50)
+ada_boost_model = AdaBoostClassifier(estimator=model, n_estimators=50, algorithm = 'SAMME')
 
 # Train the AdaBoost model on the training set
 ada_boost_model.fit(train_features, train_labels)
