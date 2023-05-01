a350cd44c2828c0f8bd83e8aa54ea71ebfdec5dd
Sam Albertson <samalbertson3@gmail.com>
2023-05-01 11:51:39 -0400

    messing with Dropout layers

--- test_cnn.py
+++ test_cnn.py
@@ -13,12 +13,14 @@ input_shape = (224, 224, 3)
 def train_model(input_shape):
     # Load the Stanford Dogs dataset
     print("Loading dogs...")
-    dogs_ds, dogs_info = tfds.load("stanford_dogs", with_info=True, split="train[:80%]")
+    dogs_ds, test_dogs_ds = tfds.load(
+        "stanford_dogs", with_info=False, split=["train[:80%]", "test"]
+    )
 
     print("Loading non-dogs...")
     # Load the Caltech 101 dataset
-    non_dogs_ds, non_dogs_info = tfds.load(
-        "caltech101", with_info=True, split="train[:80%]"
+    non_dogs_ds, test_non_dogs_ds = tfds.load(
+        "caltech101", with_info=False, split=["train[:80%]", "test"]
     )
 
     print("Processing dogs...")
@@ -27,6 +29,10 @@ def train_model(input_shape):
         lambda x: (tf.image.resize(x["image"], (224, 224)), tf.constant(1))
     )
     dogs_ds = dogs_ds.map(lambda x, y: (tf.cast(x, tf.float32) / 255.0, y))
+    test_dogs_ds = test_dogs_ds.map(
+        lambda x: (tf.image.resize(x["image"], (224, 224)), tf.constant(1))
+    )
+    test_dogs_ds = test_dogs_ds.map(lambda x, y: (tf.cast(x, tf.float32) / 255.0, y))
 
     print("Processing non-dogs...")
     # Preprocess the non-dog images
@@ -37,20 +43,35 @@ def train_model(input_shape):
         lambda x: (tf.image.resize(x["image"], (224, 224)), tf.constant(0))
     )
     non_dogs_ds = non_dogs_ds.map(lambda x, y: (tf.cast(x, tf.float32) / 255.0, y))
+    test_non_dogs_ds = test_non_dogs_ds.filter(
+        lambda x: x["label"] != 37
+    )  # exclude the "dog" class from Caltech 101
+    test_non_dogs_ds = test_non_dogs_ds.map(
+        lambda x: (tf.image.resize(x["image"], (224, 224)), tf.constant(0))
+    )
+    test_non_dogs_ds = test_non_dogs_ds.map(
+        lambda x, y: (tf.cast(x, tf.float32) / 255.0, y)
+    )
 
     # Shuffle and batch the dataset
     dogs_ds = dogs_ds.shuffle(1024).prefetch(tf.data.AUTOTUNE)
     non_dogs_ds = non_dogs_ds.shuffle(1024).prefetch(tf.data.AUTOTUNE)
+    test_dogs_ds = test_dogs_ds.shuffle(1024).prefetch(tf.data.AUTOTUNE)
+    test_non_dogs_ds = test_non_dogs_ds.shuffle(1024).prefetch(tf.data.AUTOTUNE)
 
     print("Subsetting data...")
     # Subset both datasets
     dogs_ds = dogs_ds.take(1000)
     non_dogs_ds = non_dogs_ds.take(1000)
+    test_dogs_ds = test_dogs_ds.take(1000)
+    test_non_dogs_ds = test_non_dogs_ds.take(1000)
 
     print("Finalizing image processing...")
     # Concatenate the dog and non-dog datasets
     dataset = dogs_ds.concatenate(non_dogs_ds)
     dataset = dataset.shuffle(1024).batch(32).prefetch(tf.data.AUTOTUNE)
+    test_dataset = test_dogs_ds.concatenate(test_non_dogs_ds)
+    test_dataset = test_dataset.shuffle(1024).batch(32).prefetch(tf.data.AUTOTUNE)
 
     # for image, label in dataset.take(10):
     #     # Convert the image to a numpy array
@@ -64,8 +85,8 @@ def train_model(input_shape):
     # Create the model
     model = models.Sequential()
     model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=input_shape))
-    model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation="relu", input_shape=input_shape))
+    model.add(layers.Dropout(0.3))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Flatten())
     model.add(layers.Dense(1024, activation="relu"))
@@ -83,7 +104,7 @@ def train_model(input_shape):
     )
 
     # Train the model
-    history = model.fit(dataset, epochs=10)
+    history = model.fit(dataset, epochs=5, validation_data=test_dataset)
 
     print("Done!")
     return model
